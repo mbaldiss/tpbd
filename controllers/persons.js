dd78b3e208f8f538d00d1fa6a8a29b7981699220
Marco Baldissone <marcol2c4@gmail.com>
2022-11-04 17:18:38 -0300

    update

--- controllers/persons.js
+++ controllers/persons.js
@@ -1,13 +1,15 @@
 const personsRouter = require("express").Router();
 const Person = require("../models/person");
 
+// buscar todas las personas
 personsRouter.get("/", (request, response) => {
   Person.find({}).then((persons) => {
     response.json(persons);
   });
 });
 
-personsRouter.get("/:id", (request, response, next) => {
+// buscar persona por id
+personsRouter.get("/id/:id", async (request, response, next) => {
   Person.findById(request.params.id)
     .then((person) => {
       if (person) {
@@ -19,19 +21,39 @@ personsRouter.get("/:id", (request, response, next) => {
     .catch((error) => next(error));
 });
 
-personsRouter.post("/", (request, response, next) => {
-  const body = request.body;
-
-  const person = new Person({ ...body });
-
-  person
-    .save()
-    .then((savedPerson) => {
-      response.json(savedPerson);
+// buscar persona por dni
+personsRouter.get("/dni/:dni", async (request, response, next) => {
+  Person.find({dni: request.params.dni})
+    .then((person) => {
+      if (person) {
+        response.json(person);
+      } else {
+        response.status(404).end();
+      }
     })
     .catch((error) => next(error));
 });
 
+
+// agregar persona
+personsRouter.post("/", async (request, response, next) => {
+  const body = request.body;
+  const result = await Person.find({ dni: request.body.dni });
+  if (result.length === 0) {
+    const person = new Person({ ...body });
+
+    person
+      .save()
+      .then((savedPerson) => {
+        response.json(savedPerson);
+      })
+      .catch((error) => next(error));
+  } else {
+    response.status(404).send("DNI existente.");
+  }
+});
+
+// borrar persona por id
 personsRouter.delete("/:id", (request, response, next) => {
   Person.findByIdAndRemove(request.params.id)
     .then(() => {
@@ -40,6 +62,7 @@ personsRouter.delete("/:id", (request, response, next) => {
     .catch((error) => next(error));
 });
 
+// modifica persona por id
 personsRouter.put("/:id", (request, response, next) => {
   const body = request.body;
 
--- controllers/processes.js
+++ controllers/processes.js
@@ -1,12 +1,14 @@
 const processesRouter = require("express").Router();
 const Process = require("../models/process");
 
+// buscar todos los tramites
 processesRouter.get("/", (request, response) => {
   Process.find({}).then((persons) => {
     response.json(persons);
   });
 });
 
+// buscar tramite por id
 processesRouter.get("/:id", (request, response, next) => {
   if(request.params.id === "type"){
     Process.distinct("type")
@@ -31,19 +33,26 @@ processesRouter.get("/:id", (request, response, next) => {
   }
 });
 
+// guarda tramite
 processesRouter.post("/", (request, response, next) => {
   const body = request.body;
 
-  const person = new Process({ ...body });
+  if(body.type === "Empresa" || body.type === "Particular" || body.type === "Familiar"){
+    const person = new Process({ ...body });
 
-  person
-    .save()
-    .then((savedPerson) => {
-      response.json(savedPerson);
-    })
-    .catch((error) => next(error));
+    person
+      .save()
+      .then((savedPerson) => {
+        response.json(savedPerson);
+      })
+      .catch((error) => next(error));
+  }else{
+    response.status(404).send("tipo de tramite invalido");
+  }
+
 });
 
+// borra tramite por id
 processesRouter.delete("/:id", (request, response, next) => {
   Process.findByIdAndRemove(request.params.id)
     .then(() => {
@@ -52,6 +61,7 @@ processesRouter.delete("/:id", (request, response, next) => {
     .catch((error) => next(error));
 });
 
+// modifica tramite por id
 processesRouter.put("/:id", (request, response, next) => {
   const body = request.body;
 
